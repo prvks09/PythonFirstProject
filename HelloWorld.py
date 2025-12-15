88341fdd0031ac6f36e36bc7bfe03fbcc9fe0534
Vikas Puri <58838187+prvks09@users.noreply.github.com>
2025-12-15 14:17:57 +0530

    HelloWorld updated

---
+++ HelloWorld.py
@@ -0,0 +1,44 @@
+import math
+i = 3
+print("Hello World  😊")
+print("* * * " * 10)
+print("Hello World")
+
+i = 2
+print(i)
+if i < 5:
+    print("i is less than 5")
+else:
+    print("i is 5 or more")
+k = 3
+print(len("Hello World"))
+
+course = "Python Programming"
+# course[1] = "J"
+print(course)
+course1 = "Python \\Programming"
+print(course1)
+course2 = "Python \nProgramming"
+print(course2)
+course3 = "Python \\nProgramming"
+print(course3)
+first = "Vikas"
+last = "Puri"
+full = first + " " + last
+print(full)
+full_name = f"{first} {last} is learning Python"
+print(full_name)
+print(full_name.capitalize())
+print(full_name.upper())
+print(full_name.isupper())
+print(full_name)
+
+x = 3
+x += 2
+print(x)
+
+y = 3.3
+print(math.ceil(y))
+z = print(input("Enter your name: "))
+k = f"{z} + {"2"}"
+print(k)
